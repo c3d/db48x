c5b68d58d343336911758a175d57b009dc5e4890
Christophe de Dinechin <christophe@dinechin.org>
2025-05-21 01:41:51 +0200

    cleaner: Clear command when in temporaries
    When we put a command that is in the temporaries area, we need the
    cleaner class to clear the command.
    
    In the example given in #1485, we parse a temporary from a unit object
    that is then in the purged area, but not above the end result.
    
    Fixes: #1485
    
    Signed-off-by: Christophe de Dinechin <christophe@dinechin.org>

--- src/runtime.cc
+++ src/runtime.cc
@@ -2116,7 +2116,7 @@ object_p cleaner::adjust(object_p temp)
         memmove((void *) temporaries, temp, sz);
         if (size_t scsz = rt.Editing + rt.Scratch)
             rt.move(temporaries + sz, rt.Temporaries, scsz, 1, 1);
-        if (rt.command() >= temp)
+        if (rt.command() >= temporaries)
             rt.command(nullptr);
         temp = temporaries;
         rt.Temporaries = temp + sz;
