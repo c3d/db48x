7f62a89b1061df6fdbd14c207e498dc2d4b31286
Ed van Gasteren <Ed@vanGasteren.net>
2026-05-14 21:57:40 +0200

    Make keymap files load completely.
    The keymap files (.../config/*.48k) did not load completely.
    For some planes the loading stopped early.
    
    This was due to incorrect handling of the comment character "@".
    Even a "@", a quoted At character, was treated as start of comment.
    So everything behind it on the same line got skipped.
    This also causes an inbalance of the number of double quote characters.
    
    There was a weird attempt to work around this with a "!' string in the
    keymap files.  It has a leading double and a trailing single quote!
    
    Fixed the comment handling.
    Fixed the weird trailing single quote.
    
    Added internal documentation to db48x.48k.
    Corrected the plane numbering and description.
    Corrected a few incorrect mappings (according to the keyboard overlay).
    
    Fixes: #1693
    
    Review fixups:
    * Changed alfa to alpha
    * Made plane numbers in db48x.48k 0-based, consistent with other files
    * Replaced UIDecimal in alpha mode with "." (not numerical)
    
    Signed-off-by: Ed@vanGasteren.net
    Reviewed-by: Christophe de Dinechin <christophe@dinechin.org>
    Tested-by: Christophe de Dinechin <christophe@dinechin.org>

--- src/user_interface.cc
+++ src/user_interface.cc
@@ -6288,7 +6288,7 @@ bool user_interface::load_keymap(cstring name)
     while (kmap.valid())
     {
         unicode c = kmap.get();
-        if (c == '@')
+        if (c == '@' && !quoted)
         {
             do { c = kmap.get(); } while (c && c != '\n');
             continue;
